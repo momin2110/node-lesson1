39bcce6e94c36c9568826a8c90a4d93d9abdc48d
momin-ahmed <mominahmed3040@gmail.com>
2023-06-30 00:15:05 +0600

    NodeJs tutorial lesson1 commited

---
+++ index.js
@@ -0,0 +1,16 @@
+//const user = require('./users');
+const {usersName, usersAge, cgpa} = require('./users')
+
+// console.log(user.usersName());
+// console.log(user.usersAge());
+
+// console.log(user.name());
+// console.log(user.age());
+// console.log(user.results);
+
+// console.log(user.usersName());
+// console.log(user.usersAge());
+// console.log(user.cgpa);
+console.log(usersName());
+console.log(usersAge());
+console.log(cgpa);
---
+++ users.js
@@ -0,0 +1,20 @@
+const usersName = () =>{
+    return "Momin Ahmed"
+};
+const usersAge = () =>{
+    return 33
+};
+const cgpa = 4.5;
+
+// exports.usersName = usersName;
+// exports.usersAge = usersAge;
+// exports.name = usersName;
+// exports.age = usersAge;
+// exports.results = cgpa;
+
+module.exports = {
+    usersName,
+    usersAge,
+    cgpa
+}
+
